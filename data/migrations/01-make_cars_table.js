afbab2ad9d844031a59713fc9b733aae7ee22371
Sadia Gray <sadiagray17@gmail.com>
2024-02-03 16:34:30 -0600

    set up json, migration and seed files

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,21 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).notNullable().unique()
+
+    tbl.string('make', 100).notNullable()
+
+    tbl.string('model', 100).notNullable()
+
+    tbl.numeric('mileage').notNullable()
+
+    tbl.string('title', 100)
+
+    tbl.string('transmission', 100)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
-// STRETCH
+const cars = [
+    {
+        vin: '11111111111111111',
+        make: 'subaru',
+        model: 'outback',
+        mileage: 21500,
+        title: 'clean',
+        transmission: 'automatic',
+    },
+    {
+        vin: '11111111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 1500,
+        title: 'clean',
+        transmission: 'automatic',
+    },
+    {
+        vin: '11111111111111111',
+        make: 'chevy',
+        model: 'equinox',
+        mileage: 131542,
+        title: 'salvage',
+    },
+]
+
+exports.seed = function (knex) {
+    return knex('cars')
+        .truncate().then(() => {
+            return knex('cars').insert(cars)
+        })
+}
