03e68e316c7d291ccab3497362768c4bf39b2073
Sadia Gray <sadiagray17@gmail.com>
2024-02-03 19:11:17 -0600

    carPayload middleware

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,6 +2,9 @@ const express = require('express')
 const Car = require('./cars-model')
 const {
     checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
 } = require('./cars-middleware')
 
 const router = express.Router()
@@ -19,7 +22,12 @@ router.get('/:id', checkCarId, async (req, res, next) => {
     res.json(req.car)
 })
 
-router.post('/', async (req, res, next) => {
+router.post(
+    '/',
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+    async (req, res, next) => {
     res.json('posting new car')
 })
 
